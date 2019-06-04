11c76c66c540ff1c93557c03655b976b88eccb2c
Chen Liang <chenliang5518@mails.jlu.edu.cn>
2019-06-04 21:12:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,13 +18,10 @@
 using namespace std;
 
 map<char,string> codes;
-void getcodes(Node*p,string co=""){
-
-    
+void getcodes(Node*p,string co=""){     //构建哈弗曼树的函数
     if(p->left==NULL&&p->right==NULL)
     {
         codes[p->a]=co;
-       // cout<<p->a<<":"<<p->quote<<":"<<co<<endl;
         return;}
     
     if(p->left)
@@ -34,7 +31,7 @@ void getcodes(Node*p,string co=""){
         getcodes(p->right,co+"1");
 }
 
-string turnachar(unsigned char c)
+string turnachar(unsigned char c)       //将字符转换成8位二进制串的函数
 {
     string k="";
     int j=128; //后八位为 1000_0000
@@ -52,9 +49,9 @@ string turnachar(unsigned char c)
 
 int main(int argc, const char * argv[]) {
     
-    //第一步，读取频率
+    //第一步，读取频率表
     map<char,int> frequency;
-    FILE * p =fopen("preorder.in", "r");
+    FILE * p =fopen("source.in", "r");
     char temp='a';
     while (1) {
         temp=fgetc(p);
@@ -65,14 +62,14 @@ int main(int argc, const char * argv[]) {
     }
     
     //第二步，哈夫曼编码
-    //int nums=(int)frequency.size();
-    vector<Node*> Huffman;
+    
+    vector<Node*> Huffman;  //用vector实现哈夫曼树的构建
     
     map<char,int>::iterator it;
     it = frequency.begin();
     
     
-    while(it != frequency.end())
+    while(it != frequency.end())        //首先将所有字符作为树，构建森林
     {
         Node * tempnode=new Node;
         tempnode->quote=it->second;
@@ -86,19 +83,18 @@ int main(int argc, const char * argv[]) {
     
     
     
-    sort(Huffman.begin(), Huffman.end(), compare);//保持递增序，每次选前两个结点
+    sort(Huffman.begin(), Huffman.end(), compare);// vector中森林按照compare方法（tree.h中定义）排序，保持出现次数递增
     
     while (Huffman.size()>1) {
         
-        Node * tempnode=new Node;
-        
+        Node * tempnode=new Node;//将森林中出现次数最高的两个结点（由于排过序，选vector中前两个即可）作为一个新节点的左右儿子，新节点的出现次数为儿子的次数的和，反复直到只剩一棵树。
         tempnode->left=Huffman[0];
         tempnode->right=Huffman[1];
         tempnode->quote=Huffman[0]->quote+Huffman[1]->quote;
-        
         Huffman.push_back(tempnode);
+        Huffman.erase(Huffman.begin());//弹出前两个结点
         Huffman.erase(Huffman.begin());
-        Huffman.erase(Huffman.begin());
+        
     }
     //树造好了，需要遍历编码
     getcodes(Huffman[0]);
@@ -106,24 +102,21 @@ int main(int argc, const char * argv[]) {
     
     //第三步 压缩
     
-    ofstream file("zipped.out",ios_base::trunc|ios_base::binary);
-    
-    file<<codes.size()<<endl;
+    ofstream file("zipped.out",ios_base::trunc|ios_base::binary);   //zipped.out 为压缩文件
+    file<<codes.size()<<endl;//输入共有多少种字符
     map<char,string>::iterator it1;
-    
     for (it1=codes.begin(); it1!=codes.end(); it1++) {
-        
         file<<it1->first<<it1->second<<" ";
-    }
+    }//将编码方式写入
     
-    file<<endl;
+    file<<endl;//空一行，也可以不空，我这里有点强迫症
     
-    
+    //开始写正文
     rewind(p);
     char tempc;
     string finals="";
     
-    while(1){
+    while(1){                   //首先将翻译好的源文件读入字符串finals，这里没有注意长度，你可以加对长度的限制，string最对容纳4G字符，利用map的查找
         tempc=fgetc(p);
         if(tempc!=EOF)
         {
@@ -133,7 +126,8 @@ int main(int argc, const char * argv[]) {
             break;
     }
     
-    //写入文件按bit
+    //关键
+    //按编码方式按位写入文件
     
     char ch;
     string s;
@@ -142,7 +136,7 @@ int main(int argc, const char * argv[]) {
     string str="";
     unsigned char byte;
     unsigned long tempp;
-    
+                                    
     while(length>=8)
     {
         //取前8b
@@ -192,7 +186,7 @@ int main(int argc, const char * argv[]) {
         //假如文件打开失败
 
         //要以文本文件形式打开并写入输出文件，不然回车换行不能正常显示
-        ofstream out("unzipped.out",ios_base::trunc);
+        ofstream out("unzipped.out",ios_base::trunc);           //unzipped.out 为解压文件
 
     
         map<string,char>map4;
